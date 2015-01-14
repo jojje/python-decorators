ecc4de15794498d1cbf66747cfbf23d5d5a84d90
Jonas Tingeborn <tinjon@gmail.com>
2015-01-14 07:22:31 +0100

    add support for user provided functions in place of attribute names
    * functions can be now be provided in place of attribute names in
      the decorator arg list
    * associated tests and documentation update

--- jojje/decorators.py
+++ jojje/decorators.py
@@ -4,13 +4,23 @@ def comparable(*atts, **kwargs):
 
     Positional arguments:
       The arguments are pairs of strings with every odd element being
-      the the name of the attribute to be used by sorting and the even
+      the name of the attribute to be used by sorting and the even
       elements being the sorting order for the preceding attribute.
 
       Both attribute name and sorting order directives are strings.
       The attribute sorting order is one of 'asc' for ascending order
       and 'desc' for descending order.
 
+      The attribute name may be substituted for a function which
+      computes some arbitrary value instead of referring to an object's
+      attribute. If a function is provided, it will be passed 'self' and
+      is expected to return a value that can be compared to other
+      objects of the same type.
+
+      The rest of this documentation refers for simplicity only to
+      attributes, but the reader should interpret such references to
+      mean *either* an object's attribute, *or* the value computed by a
+      provided function.
 
     Keyword argument:
     recompute -- Whether or not to recompute the hash code on each
@@ -25,14 +35,15 @@ def comparable(*atts, **kwargs):
     Description:
     The object's hash value is computed based on the values bound to the
     named attributes (atts) where the order of the listed attributes
-    matter (generates different hash codes).
+    matter, in that different order of directives produce different hash
+    codes.
 
     Equality is likewise based on checking the values of the listed
     attributes.
 
     Natural sort order is determined based on the order of the specified
     attribute names and whether the attribute should be evaluated
-    (compared with the other object) in ascending or descending order.
+    (compared with another object) in ascending or descending order.
     If the first listed attribute holds the same value for both objects
     being compared, then a check of the next attribute in the atts list
     will be performed. Thus it continues until a difference is found or
@@ -40,7 +51,7 @@ def comparable(*atts, **kwargs):
     objects are determined to have an equal rank, and consequently the
     order between them is arbitrary (undefined).
 
-    Example:
+    Example 1 -- named attributes:
 
       @comparable('name', 'asc', 'age', 'desc')
       class Person:
@@ -53,13 +64,18 @@ def comparable(*atts, **kwargs):
       alice = Person('bob', 30, 'female')
       bobsr = Person('bob', 60, 'male')
 
-      bobjr == alice                -> True
-      bobjr < bobsr                 -> False
-      len( set((bobjr, alice)) )    -> 1
-      {bobjr: bobjr}[alice].gender  -> male
+      bobjr == alice           -> True
+      bobsr < bobjr            -> True
+
+      s = set((bobjr, alice))
+      len(s)                   -> 1
+      alice in s               -> True
+      bobjr in s               -> True
+      bobsr in s               -> False
+      s.pop().gender           -> male
 
     This will provide natural ordering for Person based on a person's
-    name in ascending order, followed by age in descending, order. I.e.
+    name in ascending order, followed by age in descending order. I.e.
     if two people have the same name, then age will be checked and if
     different will decide the raking of the two compared objects.
 
@@ -70,43 +86,86 @@ def comparable(*atts, **kwargs):
     person's gender differing, since only the listed attributes are
     considered for the identity and equality checks.
 
+    Example 2 -- use of partial identity function:
+
+      @comparable('name', 'asc', lambda self: len(self.gender), 'desc')
+      class Person:
+          def __init__(self, name, age, gender):
+              self.name   = name
+              self.age    = age
+              self.gender = gender
+
+      bobjr = Person('bob', 30, 'male')
+      alice = Person('bob', 30, 'female')
+      bobsr = Person('bob', 60, 'male')
+
+      bobjr != alice               -> True
+      bobsr == bobjr               -> True
+
+      s = set((bobjr, alice))
+      len(s)                       -> 2
+      alice in s                   -> True
+      bobjr in s                   -> True
+      bobsr in s                   -> True
+      [ p.gender for p in \
+        sorted([bobjr, alice]) ]   -> [female, male]
+
+    In this slight variation of the first example, the secondary sorting
+    directive is changed to a provided function that returns the length
+    of a person's gender description. This means that if two people have
+    the same name, what distinguishes them is the length of the person's
+    gender description. Two people with the same name will have their
+    comparative ranking determined by the gender description's length as
+    well, so a person with a shorter gender description will be ranked
+    higher than someone with a longer description.
+
     Note on optimization:
     The reason the recompute option exists is to provide the user
     control over which trade-off to make with regard to memory use
     versus successive lookup performance. By default an object's hash is
     looked up only once, and then cached on the attribute __hashcode__.
-    Storing this integer value takes a small mount of memory which may
-    be a concern if the use case requires handling very large amounts of
+    Storing this integer value takes up a small amount of memory which
+    may be a concern if the use case requires handling a large amount of
     objects.
 
     By instructing the decorator to recompute an object's hash code on
     each invocation thus saves a small bit of memory per object, but may
-    incur a slight to significant overhead depending on how many
-    attributes the composed hash key is composed of (each specified
+    incur a slight to significant processing overhead depending on how
+    many attributes the composed hash key consists of (each specified
     attribute equals one hash calculation) and how many successive times
     an object is looked up.
 
     The default implementation adheres to the hashable requirement of
-    Python, that "An object is hashable if it has a hash value which
+    Python, that "an object is hashable if it has a hash value which
     never changes during its lifetime", since the hash code is computed
     only once per object. If the hash code is recomputed, then care must
     be taken to  avoid changing any attributes which the hash code is
-    based on while the object resides in a data structure that relies an
+    based on while the object resides in a data structure that relies on
     object's hash code, such as a dict or set. Changing the dependent
     attributes while the object is in such data structures will make the
     data structure fail to locate the stored object.
 
     Note on side effects:
-    The decorator adds functions to the *actual class* it is attached
-    to, that is it doesn't return a wrapper with the new functions.
-    As such it is a method generator, not a delegate constructor as
-    decorators typically are.
+    Be aware that the decorator adds functions to the *actual class* it
+    is attached to, that is it doesn't return a wrapper with the new
+    functions. As such it is a method generator, not a delegate
+    constructor as decorators typically are.
     """
 
     import inspect
 
     def pairs(atts):
         return list(zip( atts[0::2], atts[1::2] ))
+
+    # Speeds up lookup by pre-determining the right mechanism to resolve
+    # the attribute value, so we can avoid doing conditional branching
+    # in tight loops (optimization)
+    def append_attr_getters(atts):
+        def isfunc(att):
+            return hasattr(att,'__call__')
+        def pick_getter(att):
+            return (lambda obj, att: att(obj)) if isfunc(att) else getattr
+        return [( att, order, pick_getter(att) ) for (att,order) in pairs(atts)]
 
     def validate():
 
@@ -129,9 +188,7 @@ def comparable(*atts, **kwargs):
     validate()
 
     recompute  = kwargs['recompute'] if ('recompute') in kwargs else False
-    att_orders = pairs(atts)
-    att_names  = atts[0::2]
-
+    att_list   = append_attr_getters(atts)
 
     def add_methods(cls):
 
@@ -140,8 +197,8 @@ def comparable(*atts, **kwargs):
                 raise ValueError("compared objects not of the same type")
 
             ret = None
-            for att, order in att_orders:
-                a, b = getattr(self, att), getattr(other, att)
+            for att, order, geta in att_list:
+                a, b = geta(self, att), geta(other, att)
 
                 if order == 'desc':
                     b, a = a, b
@@ -154,15 +211,15 @@ def comparable(*atts, **kwargs):
         def eq(self,other):
             if not isinstance(other, self.__class__):
                 return False
-            for a in att_names:
-                if not getattr(self, a) == getattr(other, a):
+            for att, order, geta in att_list:
+                if not geta(self, att) == geta(other, att):
                     return False
             return True
 
         def compute_hash(self):
             ret = 0
-            for a in att_names:
-                h = hash( repr( getattr(self, a) ) )
+            for att, order, geta in att_list:
+                h = hash( repr( geta(self, att) ) )
                 ret ^= ret + h
             return ret
 
--- tests/decorators_test.py
+++ tests/decorators_test.py
@@ -1,6 +1,5 @@
 from jojje.decorators import comparable
 from sure import expect
-import sys, os
 
 def create_class(*decorator_args):
     @comparable(*decorator_args)
@@ -16,19 +15,19 @@ def create_class(*decorator_args):
 def create_people(Person):
     bobjr = Person('bob', 30, 'male')
     alice = Person('bob', 30, 'female')
-    bobsr = Person('bob', 60, 'male')    
+    bobsr = Person('bob', 60, 'male')
     return (bobjr, alice, bobsr)
 
-def test_identity():
+def test_identity_with_attribute():
     Person = create_class('name', 'asc', 'age', 'asc')
     (bobjr, alice, bobsr) = create_people(Person)
-    
+
     expect( bobjr ).should.eql( alice )
     expect( bobjr.__hash__() ).to.eql( alice.__hash__() )
     expect( len(set((bobjr, alice))) ).to.eql( 1 )
     expect( {bobjr: bobjr}[alice].gender ).to.eql( 'male' )
-    
-def test_compare():
+
+def test_compare_with_attribute():
     Person = create_class('age', 'asc', 'gender', 'asc' )
     (bobjr, alice, bobsr) = create_people(Person)
     expect( bobjr ).to.be.lower_than( bobsr )
@@ -36,9 +35,9 @@ def test_compare():
     Person = create_class('age', 'desc', 'gender', 'asc' )
     (bobjr, alice, bobsr) = create_people(Person)
     expect( bobjr ).to.be.greater_than( bobsr )
-    
+
     Person = create_class('name', 'asc', 'age', 'asc')
-    (bobjr, alice, bobsr) = create_people(Person)    
+    (bobjr, alice, bobsr) = create_people(Person)
     expect( bobjr ).to.be.lower_than( bobsr )
 
     Person = create_class('name', 'asc', 'age', 'desc')
@@ -48,3 +47,20 @@ def test_compare():
     Person = create_class('name', 'asc', 'gender', 'asc')
     (bobjr, alice, bobsr) = create_people(Person)
     expect( alice ).to.be.lower_than( bobjr )
+
+def test_compare_with_function():
+    Person = create_class('name', 'asc', lambda self: len(self.gender), 'asc')
+    (bobjr, alice, bobsr) = create_people(Person)
+
+    expect( bobjr ).to.be.lower_than( alice )
+    expect( bobjr ).to.eql( bobsr )
+
+def test_identity_with_function():
+    Person = create_class('name', 'asc', lambda self: len(self.gender), 'asc')
+    (bobjr, alice, bobsr) = create_people(Person)
+
+    s = set((bobjr,alice,bobsr))
+
+    expect( s ).to.have.length_of(2)
+    expect( [p.age for p in s] ).to.eql([30,30])
+    expect( [p.gender for p in sorted(s)] ).to.eql([ 'male', 'female' ])
